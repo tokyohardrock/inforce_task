48543ca5e3337e8bc1df9c07b8b4c06374342348
Oleksii Petrushyn <tokyohardrock@gmail.com>
2026-08-06 14:23:24 +0300

    feat: add Content-Type validation

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,10 +5,29 @@ import (
 	"fmt"
 	"inforce_task/internal/model"
 	"log/slog"
+	"mime"
 	"net/http"
 )
 
+func IsJSONContentType(contentType string) bool {
+	if contentType == "" {
+		return false
+	}
+
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return false
+	}
+
+	return mediaType == "application/json"
+}
+
 func CreateEvent(w http.ResponseWriter, r *http.Request) {
+	if !IsJSONContentType(r.Header.Get("Content-Type")) {
+		http.Error(w, "Content-Type must be application/json", http.StatusUnsupportedMediaType)
+		return
+	}
+
 	event := new(model.ActivityEvent)
 
 	err := json.NewDecoder(r.Body).Decode(event)
