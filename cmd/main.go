ae00f9d39f725c48143bcd5e66c10d42af37055c
Oleksii Petrushyn <tokyohardrock@gmail.com>
2026-08-06 13:45:02 +0300

    feat: add entrypoint

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"inforce_task/internal/config"
+	"inforce_task/internal/handler"
+	"inforce_task/internal/logger"
+	"inforce_task/internal/server"
+	"log/slog"
+	"net/http"
+)
+
+func main() {
+	log := logger.InitLogger()
+
+	slog.SetDefault(log)
+
+	cfg, err := config.MustLoad()
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /events", handler.CreateEvent)
+
+	server, err := server.New(&cfg.Server, mux)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	fmt.Println("Server runs on ", server.Addr)
+
+	if err = server.ListenAndServe(); err != nil {
+		slog.Error(err.Error())
+		return
+	}
+}
